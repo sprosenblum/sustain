8ba0eafd418a3a05252439ef29884e4decc50262
Sarah Rosenblum <81576433+sprosenblum@users.noreply.github.com>
2022-08-16 15:39:01 -0400

    Update header.php

--- header.php
+++ header.php
@@ -9,5 +9,9 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
         <title><?php wp_title( '|', true, 'right' ); ?></title>
         <link rel="stylesheet" href="<?php echo esc_url( get_stylesheet_uri() ); ?>" type="text/css" />
-        <?php wp_head(); ?>
+
 </head>
+
+<!-- add navbar -->
+
+<?php wp_head(); ?>
