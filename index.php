10f03eb06fb4b053371f315cb75d947e5f977c75
Sarah Rosenblum <81576433+sprosenblum@users.noreply.github.com>
2022-08-17 15:41:30 -0400

    navbar styling

--- index.php
+++ index.php
@@ -16,7 +16,9 @@ get_header();
         <?php if ( have_posts() ) :
                 while ( have_posts() ) : the_post(); ?>
 
-                    <h3><?php the_title(); ?></h3>
+                    <h3>
+                        <?php the_title(); ?>
+                    </h3>
 
                     <?php the_content(); ?>
                     <?php wp_link_pages(); ?>
